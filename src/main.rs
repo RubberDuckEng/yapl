af118600dd84b3862a3b281a561629a87fc89bbb
Adam Barth <abarth@chromium.org>
2022-08-28 15:19:33 -0700

    Add rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline;
+
+fn main() -> rustyline::Result<()> {
+    let mut rl = rustyline::Editor::<()>::new()?;
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => println!("Line: {:?}", line),
+            Err(rustyline::error::ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(rustyline::error::ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => println!("Error: {:?}", err),
+        }
+    }
+    Ok(())
 }
